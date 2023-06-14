a0a086338eb30d4fa429a262bd9ac09aed954ba2
toho36 <toho0036@gmail.com>
2023-06-14 13:22:48 +0200

    not working recipe

---
+++ src/pages/Recipe.js
@@ -0,0 +1,106 @@
+import React from 'react';
+import { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import { useParams } from 'react-router-dom';
+
+function Recipe() {
+  let params = useParams();
+  const [details, setDetails] = useState({});
+  const [activeTab, setActiveTab] = useState('instructions');
+
+  const fetchDetails = async () => {
+    try {
+      const data = await fetch(
+        `https://react-http-98ba6-default-rtdb.europe-west1.firebasedatabase.app/recipes/${params.name}.json`
+      );
+      const detailData = await data.json();
+      console.log(detailData);
+      setDetails(detailData || {});
+    } catch (error) {
+      console.error('Error fetching recipes:', error);
+    }
+  };
+  useEffect(() => {
+    fetchDetails();
+  }, [params.name]);
+  const tags = details.tags || [];
+  const tagsString = tags.join(', ');
+  return (
+    <DetailWrapper>
+      <div>
+        <h2>{details.title}</h2>
+        <img src={details.image} alt="" />
+        <h2>Description: </h2>
+        <h3 dangerouslySetInnerHTML={{ __html: details.description }}></h3>
+        <h4>Tags: {tagsString}</h4>
+      </div>
+      <Info>
+        <Button
+          className={activeTab === 'ingredients' ? 'active' : ''}
+          onClick={() => setActiveTab('instructions')}
+        >
+          Instructions
+        </Button>
+        <Button
+          className={activeTab === 'instructions' ? 'active' : ''}
+          onClick={() => setActiveTab('ingredients')}
+        >
+          Ingredients
+        </Button>
+        {activeTab === 'instructions' && (
+          <div>
+            <h2>Instructions: </h2>
+            <ol>
+              {details.instructions.map((instructions) => (
+                <li>{instructions}</li>
+              ))}
+            </ol>
+          </div>
+        )}
+        {activeTab === 'ingredients' && (
+          <div>
+            <h2>Required ingredients</h2>
+            <ul>
+              {details.ingredients.map((ingredients) => (
+                <li>{ingredients}</li>
+              ))}
+            </ul>
+          </div>
+        )}
+      </Info>
+    </DetailWrapper>
+  );
+}
+const DetailWrapper = styled.div`
+  margin-top: 10rem;
+  margin-bottom: 5rem;
+  display: flex;
+  .active {
+    background: linear-gradient(35deg, #494949, #313131);
+    color: white;
+  }
+  h2 {
+    margin-bottom: 2rem;
+  }
+  li {
+    font-size: 1.2rem;
+    line-height: 2.5rem;
+  }
+  ul {
+    margin-top: 2rem;
+  }
+`;
+const Button = styled.button`
+  pading: 1rem 2rem;
+  color: #313131;
+  background: white;
+  border: 2px solid black;
+  margin-right: 2rem;
+  font-weight: 600;
+`;
+
+const Info = styled.div`
+  margin-left: 10rem;
+`;
+
+export default Recipe;
